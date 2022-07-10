82b7b4dbd5421ad4a471c99aeb517f580f2b49f7
Salvius-Lvov <mr.azaki2016@gmail.com>
2022-07-10 13:18:32 +0300

    change navbar Btstrp 5.2 with no css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {
+  NgModule
+} from '@angular/core';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {
+  AppRoutingModule
+} from './app-routing.module';
+import {
+  AppComponent
+} from './app.component';
+import {
+  HeaderComponent
+} from './header/header.component';
+
+import {
+  NgbModule
+} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,10 +27,11 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
     NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
